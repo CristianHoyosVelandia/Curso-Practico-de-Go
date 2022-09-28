8f750c0cff32ac9e5f3a1bce1e30d07d35b7a33f
Cristian Hoyos Velandia <cristian-hoyos@hotmail.com>
2022-09-28 11:17:46 -0500

    ciclos while for clase 27

--- goServidores.go
+++ goServidores.go
@@ -20,20 +20,19 @@ func main() {
 
 	i := 0
 
-	for i < 2 {
+	for i < 4 {
+
 		for _, servidor := range servidores {
 			go revisarServidores(servidor, channel)
 		}
-
-		for index := range servidores {
-			fmt.Println(<-channel, index+1)
-		}
 		time.Sleep(1 * time.Second)
+
 		fmt.Println(<-channel)
 		i++
 	}
+
 	tiempoPaso := time.Since(inicio)
-	fmt.Printf("Tiempo de ejecuccion %q\n", tiempoPaso)
+	fmt.Printf("Tiempo de ejecuccion %s\n", tiempoPaso)
 }
 
 func revisarServidores(servidor string, channel chan string) {
@@ -41,10 +40,10 @@ func revisarServidores(servidor string, channel chan string) {
 
 	if err != nil {
 		// fmt.Println(servidor, " No disponible :C")
-		channel <- servidor + "No disponible :C"
+		channel <- servidor + " No disponible :C"
 	} else {
 		// fmt.Println(servidor, " Funcionando correctamente :)")
-		channel <- servidor + "Funcionando correctamente :)"
+		channel <- servidor + " Funcionando correctamente :)"
 	}
 
 }
