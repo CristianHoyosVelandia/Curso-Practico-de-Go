cf5061fb10212e26a55724dd036d14dd47b324b3
Cristian Hoyos Velandia <cristian-hoyos@hotmail.com>
2022-09-30 10:23:20 -0500

    middlewares and tyoes middlewares in ws

---
+++ WebServer/types.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+//Se definió un type Middleware que recibe y
+//retorne http.HandlerFunc así todas las funciones que cuenten con esta firma se considerarán de tipo Middleware.
+
+//Reciben y retornan lo mismo, para poder encadenarse y pasar el request entre varios middleware, hasta llegar al handler.
+//Algo interesante de esta definición es como Middleware tiene un type func como parámetro,
+//significa que recibirá una función como argumento que tiene una firma definida http.HandlerFunc.
+
+//El operador elipsis (…) sirve para determinar una cantidad variable de parametros de un mismo tipo.
+type Middleware func(http.HandlerFunc) http.HandlerFunc
